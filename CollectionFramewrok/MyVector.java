affa7721df344e87b2ba0cf79717b3781b6daf78
zain17492 <109415633+zain17492@users.noreply.github.com>
2022-09-12 20:37:30 +0530

    First Commit
    First Commit

---
+++ CollectionFramewrok/MyVector.java
@@ -0,0 +1,25 @@
+package CollectionFramewrok;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class MyVector {
+	public static void main(String [] args) {
+		Vector<String> v1 = new Vector();
+		ArrayList<String> a1 = new ArrayList();
+		LinkedList l1 = new LinkedList();
+		
+		String s1 = new String("Zain");
+		String s2 = new String("Zainab");
+		String s3 = new String("Vaibhavi");
+		String s4 = new String("ABC");
+		String s5 = new String("PQR");
+		
+		v1.add(s1);
+		a1.add(s4);
+		
+		
+	}
+
+}
